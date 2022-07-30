1ba1b669621a774cbbcf1b9f09c0d31901d4efb9
Mel4221 <melqui4251@gmail.com>
2022-07-29 22:30:11 -0400

    Here i have all my notes from using bash on linux

---
+++ script.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+
+
+#----------------------------------------
+#-------------First  Class---------------
+#----------------------------------------
+
+
+#this is a makes a file an executable one 
+#chmod  +x  fileName.txt : this will make one executable 
+#chmod a+x fileName.txt : this will make it executb
+
+
+
+
+
+
+#sudo touch file.txt; 
+#sudo touch file.pdf 
+#sudo touch file.doc
+#sudo touch file.html
+#sudo touch file.xml 
+
+#clear; 
+#ls -ll; 
+
+
+#sudo rm file.txt;
+#sudo rm file.pdf; 
+#sudo rm file.doc; 
+#sudo rm file.html;
+#sudo rm file.xml; 
+
+#ls -ll;
+#echo 'Created at'; date; 
+
+#--------------------------------------
+#-------------Second Class-------------
+#--------------------------------------
+
+
+
+
+
+var  = "text"; 
+
+echo $var; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
